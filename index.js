cd126358f22fde25ba01f757556c2941ab414b00
reshad21 <miakala7@gmail.com>
2023-01-17 13:12:35 +0600

    students collection create

--- index.js
+++ index.js
@@ -4,19 +4,38 @@ const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
- 
+
 // middleware
 app.use(cors());
 app.use(express.json());
- 
- 
-app.get('/',(req,res)=>{
+
+
+app.get('/', (req, res) => {
     res.send('server is running');
 })
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gplljg9.mongodb.net/?retryWrites=true&w=majority`;
 
- 
-app.listen(port,()=>{
+async function run() {
+    try {
+        const studentsCollection = client.db("softnerveDb").collection("students");
+
+        // students collection code
+        app.post('/student',async(req,res)=>{
+            const items = req.body;
+            const result = await haiku.studentsCollection(items);
+            res.send(result);
+        })
+
+       
+    } 
+    finally {
+        
+    }
+}
+run().catch(console.dir);
+
+
+app.listen(port, () => {
     console.log(`Listening to the port ${port}`);
 })
